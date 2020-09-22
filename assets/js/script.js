ef63c70fab8ff4db6903c5b8c7b460c47561057e
Siwei Sun <ssun25@id.iit.edu>
2020-09-22 10:08:12 -0500

    pushtogithub

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,9 +2,8 @@ const pressme = document.querySelector('.add-button');
 
 let text = 'pressme';
 
-function clickMe(e) {
+function clickMe() {
   if (typeof text == 'string') {
-    console.log(1);
     text = 1;
   } else {
     text += 1;
